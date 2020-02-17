df05389decbd7d3c7344edf10a6674a67755ef10
Mario Martinez <mmartinez53@ucol.mx>
2020-02-17 13:47:03 -0600

    Commented instances in application.js file

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,8 +10,8 @@ require("channels")
 
 require("jquery")
 require("turbolinks")
-require("materialize-sprockets")
-require("materialize/extras/nouislider")
+// require("materialize-sprockets")
+// require("materialize/extras/nouislider")
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
