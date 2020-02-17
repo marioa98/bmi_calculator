a12dee45008fca9d90f60e5afa6b7e40f04e96cc
Mario Martinez <mmartinez53@ucol.mx>
2020-02-16 18:30:20 -0600

    Partial changes for test files

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,18 +2,18 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get login" do
-    get users_login_url
+    post "/login", user: {names:"", surnames: "",email: "root@domain.com", password: "123456"}
     assert_response :success
   end
 
-  test "should get signin" do
-    get users_signin_url
-    assert_response :success
-  end
+# test "should get signin" do
+#   get users_signin_url
+#   assert_response :success
+# end
 
-  test "should get showcase" do
-    get users_showcase_url
-    assert_response :success
-  end
+# test "should get showcase" do
+#   get users_showcase_url
+#   assert_response :success
+# end
 
 end
